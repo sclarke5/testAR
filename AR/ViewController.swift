9821c2069e55632af6dcf027c979f23698fddc16
sclarke5 <sclarke5555@gmail.com>
2021-01-05 13:58:03 -0500

    basic tap-and-render functionality for 3D object

--- AR/ViewController.swift
+++ AR/ViewController.swift
@@ -22,11 +22,38 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTap(_:)))
+        sceneView.addGestureRecognizer(tapGesture)
         
-        // Set the scene to the view
-        sceneView.scene = scene
+    }
+    
+    @objc
+    func didTap(_ gesture: UITapGestureRecognizer){
+        let sceneViewTappedOn = gesture.view as! ARSCNView
+        let touchCoordinates = gesture.location(in: sceneViewTappedOn)
+        let hitTest = sceneViewTappedOn.hitTest(touchCoordinates, types: .existingPlaneUsingExtent)
+
+        guard !hitTest.isEmpty, let hitTestResult = hitTest.first else {
+            return
+        }
+        
+        let position = SCNVector3(hitTestResult.worldTransform.columns.3.x, hitTestResult.worldTransform.columns.3.y, hitTestResult.worldTransform.columns.3.z)
+        
+        addItemToPosition(position)
+    }
+    
+    func addItemToPosition(_ position: SCNVector3){
+        guard let url = Bundle.main.url(forResource: "cup_saucer_set", withExtension: "usdz", subdirectory: "art.scnassets") else { return }
+        
+        let scene = try! SCNScene(url: url, options: [.checkConsistency: true])
+        
+        DispatchQueue.main.async {
+            if let node = scene.rootNode.childNode(withName: "cup_saucer_set", recursively: false){
+                node.position = position
+                self.sceneView.scene.rootNode.addChildNode(node)
+            }
+            
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -34,6 +61,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        
+        // Enable horizontal plane detection
+        configuration.planeDetection = .horizontal
+        
+        // Show feature points
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -48,14 +81,34 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     // MARK: - ARSCNViewDelegate
     
-/*
+
     // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        if let planeAnchor = anchor as? ARPlaneAnchor {
+            
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            plane.firstMaterial?.diffuse.contents = UIColor(white: 1, alpha: 0.075)
+            
+            let planeNode = SCNNode(geometry: plane)
+            planeNode.position = SCNVector3Make(planeAnchor.center.x, planeAnchor.center.x, planeAnchor.center.z)
+            planeNode.eulerAngles.x = -.pi / 2
+            
+            node.addChildNode(planeNode)
+        }
     }
-*/
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        
+        if let planeAnchor = anchor as? ARPlaneAnchor,
+           let planeNode = node.childNodes.first,
+           let plane = planeNode.geometry as? SCNPlane {
+            plane.width = CGFloat(planeAnchor.extent.x)
+            plane.height = CGFloat(planeAnchor.extent.z)
+            planeNode.position = SCNVector3Make(planeAnchor.center.x, 0, planeAnchor.center.z)
+        }
+    }
+
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
